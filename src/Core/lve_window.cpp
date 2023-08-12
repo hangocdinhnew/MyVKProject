7cf5b1e8870eb3fb6a13cfac7f0886e13a48335a
Dinh Ha Ngoc <dinhndrwnew@gmail.com>
2023-08-12 21:57:37 +0700

    feat(add): adding Vulkan Devices

--- src/Core/lve_window.cpp
+++ src/Core/lve_window.cpp
@@ -20,4 +20,13 @@ void LveWindow::initWindow() {
   window =
       glfwCreateWindow(width, height, windowName.c_str(), nullptr, nullptr);
 }
+
+void LveWindow::createWindowSurface(VkInstance instance,
+                                    VkSurfaceKHR *surface) {
+  if (glfwCreateWindowSurface(instance, window, nullptr, surface) !=
+      VK_SUCCESS) {
+    throw std::runtime_error("failed to craete window surface");
+  }
+}
+
 } // namespace lve
