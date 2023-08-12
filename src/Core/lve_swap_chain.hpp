1926bfab3c10c96e13ccda3254cfaf5fa736b396
Dinh Ha Ngoc <dinhndrwnew@gmail.com>
2023-08-12 23:35:01 +0700

    feat(graphics): add Renderer & Systems

--- src/Core/lve_swap_chain.hpp
+++ src/Core/lve_swap_chain.hpp
@@ -46,6 +46,11 @@ public:
   VkResult submitCommandBuffers(const VkCommandBuffer *buffers,
                                 uint32_t *imageIndex);
 
+  bool compareSwapFormats(const LveSwapChain &swapChain) const {
+    return swapChain.swapChainDepthFormat == swapChainDepthFormat &&
+           swapChain.swapChainImageFormat == swapChainImageFormat;
+  }
+
 private:
   void init();
   void createSwapChain();
@@ -63,6 +68,7 @@ private:
   VkExtent2D chooseSwapExtent(const VkSurfaceCapabilitiesKHR &capabilities);
 
   VkFormat swapChainImageFormat;
+  VkFormat swapChainDepthFormat;
   VkExtent2D swapChainExtent;
 
   std::vector<VkFramebuffer> swapChainFramebuffers;
